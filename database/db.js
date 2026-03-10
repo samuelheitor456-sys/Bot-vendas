0a94438f1ac5b0e59466297f9759195c86544fb6
samuelheitor456-sys <amuelheitor456@gmail.com>
2026-03-10 13:41:49 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../produtos.db');
+const dbPath = path.resolve(__dirname, '../data/produtos.db');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
